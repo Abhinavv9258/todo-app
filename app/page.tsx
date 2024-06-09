b3bff1e72fea3fca810e61d35234e301dfff8c39
Abhinav Verma <79451162+Abhinavv9258@users.noreply.github.com>
2024-06-09 23:32:55 +0530

    Implement dotted background pattern

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,13 @@ import TodoList from '../components/TodoList';
 import NewTodoForm from '../components/NewTodoForm';
 
 const Home: React.FC = () => {
-  const [todo, setTodo] = useState<TodoItem[]>([]);
-
-  return (
-    <div className="min-h-screen p-4 bg-gray-100">
-      <TodoList todo={todo} setTodo={setTodo} />
-      <NewTodoForm setTodo={setTodo} />
-    </div>
-  );
+    const [todo, setTodo] = useState<TodoItem[]>([]);
+    return (
+        <div className="h-screen w-screen bg-dot-gray-300 text-white">
+            <TodoList todo={todo} setTodo={setTodo} />
+            <NewTodoForm setTodo={setTodo} />
+        </div>
+    );
 };
 
 export default Home;
